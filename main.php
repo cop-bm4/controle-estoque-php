fbeb8b139e9feb3c2d2dffc352f032cc51837ea2
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 18:46:13 -0200

    importar bibliotecas

--- main.php
+++ main.php
@@ -18,7 +18,14 @@ if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == tru
 
 $logado = $_SESSION['login'];
 ?>
-
+<!--mod mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!---->
+  <!-- include bibliotecas bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!---->
 </head>
 
 

708b37068bd2dd60788e915fe88718e275de77af
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 09:34:40 -0300

    db

--- index.php
+++ index.php
@@ -28,9 +28,10 @@
 $servername = "mysql785.umbler.com";
 $username = "joaoaluz";
 $password = "joao99945222";
+$db = "controle_materia"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {

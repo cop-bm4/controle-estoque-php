2e332a0858f9fb11e8694b14eca0893eccb8795b
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 19:29:07 -0200

    s

--- logout.php
+++ logout.php
@@ -1,16 +1,10 @@
 <?php
 
 session_start();
-$token = md5(session_id());
-if(isset($_GET['token']) && $_GET['token'] === $token) {
-   // limpe tudo que for necessário na saída.
-   // Eu geralmente não destruo a seção, mas invalido os dados da mesma
-   // para evitar algum "necromancer" recuperar dados. Mas simplifiquemos:
-   session_destroy();
-   header("location: index.php");
-   exit();
-} else {
-   echo '<a href="logout.php?token='.$token.'>Confirmar logout</a>';
+if(isset($_SESSION['logado'])){
+    // se você possui algum cookie relacionado com o login deve ser removido
+    session_destroy();
+    header("location: http://mytestsite-com-br.umbler.net/index.php");
+    exit();
 }
-
 ?>

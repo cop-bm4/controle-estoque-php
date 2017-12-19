e10c1be9ca22170af6edec84cf577eb7b74c52cc
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 09:22:44 -0300

    conexao

--- index.php
+++ index.php
@@ -19,6 +19,29 @@
 
 
 <body>  
+
+<!-- Banco de dados conect-->
+
+
+<?php
+
+$servername = "mysql785.umbler.com";
+$username = "joaoaluz";
+$password = "joao99945222";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
+<!--conec -->
+
+
 <h1>teste</h1>
 <div id="brasao"  style='position:absolute;margin-left:1200px;margin-top:5;width:160px;height:160px;z-index:1'><img src="http://mytestsite-com-br.umbler.net/Bras%C3%A3o_CBM_PA.PNG" width="100" height="100"></div>
 

be524616f80f8f2fbe7521763b49130cb140f32f
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 11:46:34 -0300

    mysqli

--- validalogin.php
+++ validalogin.php
@@ -5,13 +5,13 @@ session_start();
 $login = $_POST['login'];
 $senha = $_POST['senha'];
 // as próximas 3 linhas são responsáveis em se conectar com o bando de dados.
-$con = mysql_connect("mysql785.umbler.com", "joaoaluz", "joao9994522") or die ("Sem conexão com o servidor");
-$select = mysql_select_db("server") or die("Sem acesso ao DB");
+$con = mysqli_connect("mysql785.umbler.com", "joaoaluz", "joao9994522") or die ("Sem conexão com o servidor");
+$select = mysqli_select_db("server") or die("Sem acesso ao DB");
 
 // A variavel $result pega as varias $login e $senha, faz uma pesquisa na tabela de usuarios
-$result = mysql_query("SELECT * FROM `USUARIO` WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
+$result = mysqli_query("SELECT * FROM `USUARIO` WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
 /* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do resultado ele redirecionará para a pagina site.php ou retornara  para a pagina do formulário inicial para que se possa tentar novamente realizar o login */
-if(mysql_num_rows ($result) > 0 )
+if(mysqli_num_rows ($result) > 0 )
 {
 $_SESSION['login'] = $login;
 $_SESSION['senha'] = $senha;

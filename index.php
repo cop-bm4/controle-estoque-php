8fc816a24bd8d0bbd5fa46b938e26065d835f448
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 18:41:08 -0200

    login/page

--- index.php
+++ index.php
@@ -22,13 +22,13 @@
       <a class="navbar-brand" href="http://mytestsite-com-br.umbler.net/"> SISCOMP </a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="http://mytestsite-com-br.umbler.net/login.html">Home</a></li>
+      <li class="active"><a href="http://mytestsite-com-br.umbler.net/">Home</a></li>
       <li><a href="#">Page 1</a></li>
       <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="http://mytestsite-com-br.umbler.net/login.html"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Sair</a></li>
     </ul>
   </div>
 </nav>

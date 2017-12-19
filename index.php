ec59cfc86a85732043f5c051f0b44926f41359ec
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 19:45:32 -0200

    botrao

--- index.php
+++ index.php
@@ -40,21 +40,22 @@
 
 
 
-</form>
+
 
 
 <div class="container">
 <form method="post" class="input-group" action="validalogin.php" id="formlogin" name="formlogin">
 
- <div class="input-group">
+ <div class="input-group input-group-lg">
     <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
     <input id="login" type="text" class="form-control" name="login" placeholder="login">
   </div>
-  <div class="input-group">
+  <div class="input-group input-group-lg">
     <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
     <input id="senha" type="password" class="form-control" name="senha" placeholder="senha">
   </div>
-  
+  <input type="submit" class="btn  btn-lg" value="LOGAR	"  />
+
   </div>
 </form>
 

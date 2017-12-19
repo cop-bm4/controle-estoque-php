4d9dcf300f86db1e0b0c503c6b74ca3548768cf4
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 09:39:52 -0300

    criando tabela de usuario

--- index.php
+++ index.php
@@ -38,8 +38,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
-?>
 
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+?>
 <!--conec -->
 
 

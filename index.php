cde1213b00dd043c20df5e4dbb7d50ca1a92b329
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 18:29:11 -0200

    s

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="http://mytestsite-com-br.umbler.net/"SISCOMP</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="http://mytestsite-com-br.umbler.net/login.html">Home</a></li>
       <li><a href="#">Page 1</a></li>
       <li><a href="#">Page 2</a></li>
     </ul>

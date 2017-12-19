29f1e2c68569bf803846547bbc72083a317dffa8
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 18:26:50 -0200

    navbar

--- index.php
+++ index.php
@@ -16,17 +16,27 @@
 
 </head>
 <body>
-
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
 <div class="container">
   <div class="jumbotron">
     <h1>SISCOMP</h1> 
     <p class="text-danger">Sistema de controle de material operacional</p> 
-  <ul class="nav nav-tabs nav-justified">
-  <li class="active"><a href="http://mytestsite-com-br.umbler.net/login.html">Login</a></li>
-  <li><a href="#">Menu 1</a></li>
-  <li><a href="#">Menu 2</a></li>
-  <li><a href="#">Menu 3</a></li>
-</ul>
+
   </div>
   
 

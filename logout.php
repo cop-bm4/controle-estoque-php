11481649ef8b792d01b20437d8e13a9099fbd1c2
joaoaluz <joao_aluz@hotmail.com>
2017-12-19 19:30:58 -0200

    s

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
 
 session_start();
-if(isset($_SESSION['logado'])){
+
     // se você possui algum cookie relacionado com o login deve ser removido
     session_destroy();
     header("location: http://mytestsite-com-br.umbler.net/index.php");
-    exit();
+
 }
 ?>

818a34153e1dc6a462e19ff87b8ee6f3acb92b4e
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 09:57:16 -0300

    ;

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 $servername = "mysql785.umbler.com";
 $username = "joaoaluz";
 $password = "joao99945222";
-$db = "controle_materia"
+$db = "controle_materia";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);

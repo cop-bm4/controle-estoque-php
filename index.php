2dfcfd8d32e36edf044b4491efeb18adcc703dec
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 09:49:29 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>  
+
+<html>
+
+<head>
+
+<style>
+
+.error {color: #FF0000;}
+
+</style>
+
+<title>Controle de Material Operacional</title>
+
+	<link rel="stylesheet" type="text/css" href="estilo.css"/>
+
+</head>
+
+
+
+<body>  
+<h1>teste</h1>
+<div id="brasao"  style='position:absolute;margin-left:1200px;margin-top:5;width:160px;height:160px;z-index:1'><img src="https://luzz.000webhostapp.com/Bras%C3%A3o_CBM_PA.PNG" width="100" height="100"></div>
+
+
+
+
+<h1>Controle de Material Operacional</h1>
+
+
+
+<form method="post" action="forms.php">  
+
+ABAFADOR INCÊNDIO <input type=text name="quantidade"><br> 
+
+<select name="situacao">
+
+<option value=Operante>Operante</option>
+
+<option value=Inoperante>Inoperante</option>
+
+</select><BR><BR>
+
+
+
+
+
+
+
+  
+
+  <input type="submit" name="submit" value="Enviar">  
+
+</form>
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>

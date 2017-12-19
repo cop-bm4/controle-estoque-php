d3aabed1e03e3a01dd064f71c1a6e661a7dac8a8
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 11:53:39 -0300

    s

--- validalogin.php
+++ validalogin.php
@@ -6,10 +6,10 @@ $login = $_POST['login'];
 $senha = $_POST['senha'];
 // as próximas 3 linhas são responsáveis em se conectar com o bando de dados.
 $con = mysql_connect("mysql785.umbler.com", "joaoaluz", "joao9994522") or die ("Sem conexão com o servidor");
-$select = mysql_select_db("server") or die("Sem acesso ao DB");
+$select = mysql_select_db("controle_materia") or die("Sem acesso ao DB");
 
 // A variavel $result pega as varias $login e $senha, faz uma pesquisa na tabela de usuarios
-$result = mysql_query("SELECT * FROM `USUARIO` WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
+$result = mysql_query("SELECT * FROM `usuarios` WHERE `login` = '$login' AND `senha`= '$senha'");
 /* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do resultado ele redirecionará para a pagina site.php ou retornara  para a pagina do formulário inicial para que se possa tentar novamente realizar o login */
 if(mysql_num_rows ($result) > 0 )
 {

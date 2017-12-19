d075e4dcd114a7bf168fe9864657bb7bac4cb3d7
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-19 09:08:08 -0300

    s

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 <body>  
 <h1>teste</h1>
-<div id="brasao"  style='position:absolute;margin-left:1200px;margin-top:5;width:160px;height:160px;z-index:1'><img src="Brasão_CBM_PA.PNG" width="100" height="100"></div>
+<div id="brasao"  style='position:absolute;margin-left:1200px;margin-top:5;width:160px;height:160px;z-index:1'><img src="http://mytestsite-com-br.umbler.net/Bras%C3%A3o_CBM_PA.PNG" width="100" height="100"></div>
 
 
 

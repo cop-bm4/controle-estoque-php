7c7b1377194d16259cdb69de66720298dabf2977
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-20 08:10:35 -0300

    login em home

--- home.php
+++ home.php
@@ -3,6 +3,19 @@
 
 
 <head>
+
+	<!--login -->
+<?php  
+session_start();
+if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
+{
+	unset($_SESSION['login']);
+	unset($_SESSION['senha']);
+	header('location:index.php');
+	}
+
+$logado = $_SESSION['login'];
+?>
 	<title>Home</title>
 		 <meta charset="utf-8">
   

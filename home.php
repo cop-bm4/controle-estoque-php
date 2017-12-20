82390c27932b52bd58b115117c3da3a8eae39dd8
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-20 08:14:50 -0300

    home.php

--- home.php
+++ home.php
@@ -11,7 +11,7 @@ if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == tru
 {
 	unset($_SESSION['login']);
 	unset($_SESSION['senha']);
-	header('location:index.php');
+	header('location:home.php');
 	}
 
 $logado = $_SESSION['login'];

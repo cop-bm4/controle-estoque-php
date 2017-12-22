c5371dfd08353eb6638a91b55c400810719fe892
joaoaluz <joao_aluz@hotmail.com>
2017-12-21 22:21:32 -0200

    v

--- grud/main.php
+++ grud/main.php
@@ -51,8 +51,8 @@ echo "Não foi possível conectar ao banco de dados";
 <!---->
 
 
-
-<div class="row">				<div class="col-sm-6">					<h2>Clientes</h2>				</div>				<div class="col-sm-6 text-right h2">			    	<a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Cliente</a>			    	<a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>			    </div>			</div>
+<!--tabela -->
+<div class="row">				<div class="col-sm-6">					<h2>Material</h2>				</div>				<div class="col-sm-6 text-right h2">			    	<a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Registro</a>			    	<a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>			    </div>			</div>
 </head>
 
 <body>

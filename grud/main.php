dec53861622ba9796b171b79f3e7b458ff95b699
joaoaluz <joao_aluz@hotmail.com>
2017-12-21 22:14:19 -0200

    main/grud

---
+++ grud/main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CONTROLE DE MATERIAL</title>
+	<!--mod mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!---->
+  <!-- include bibliotecas bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!---->
+
+</head>
+
+<body>
+<?php
+require "public/db.php"; 
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+echo "Não foi possível conectar ao banco de dados";
+} 
+
+
+?>
+<!--conec -->
+
+</body>
+</html>

70da714ae18ddd8b0d2ae5206a8d8b1a8732f95c
joaoaluz <joao_aluz@hotmail.com>
2017-12-21 22:17:43 -0200

    cabeçalho

--- grud/main.php
+++ grud/main.php
@@ -10,7 +10,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!---->
-
+<div class="row">				<div class="col-sm-6">					<h2>Clientes</h2>				</div>				<div class="col-sm-6 text-right h2">			    	<a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Cliente</a>			    	<a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>			    </div>			</div>
 </head>
 
 <body>

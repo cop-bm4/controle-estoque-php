4aca59630f3e89dde8efa379244a4b61c84c4b83
joaoaluz <joao_aluz@hotmail.com>
2017-12-21 22:20:13 -0200

    cabeloainsa

--- grud/main.php
+++ grud/main.php
@@ -2,19 +2,9 @@
 <html>
 <head>
 	<title>CONTROLE DE MATERIAL</title>
-	<!--mod mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<!---->
-  <!-- include bibliotecas bootstrap-->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!---->
-<div class="row">				<div class="col-sm-6">					<h2>Clientes</h2>				</div>				<div class="col-sm-6 text-right h2">			    	<a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Cliente</a>			    	<a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>			    </div>			</div>
-</head>
 
-<body>
-<?php
+<!-- banco de dados-->
+	<?php
 require "/home/mytestsite.com.br/public/db.php"; 
 
 // Check connection
@@ -25,6 +15,48 @@ echo "Não foi possível conectar ao banco de dados";
 
 
 ?>
+	<!--mod mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!---->
+  <!-- include bibliotecas bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!---->
+
+<!--menu-->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"> SISCOMP </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li ><a href="http://mytestsite-com-br.umbler.net/home.php">Home</a></li>
+      <li class= "active" class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="http://mytestsite-com-br.umbler.net/cadastro-material.php">Cadastro<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="http://mytestsite-com-br.umbler.net/cadastro-material.php" >Material</a></li>
+          <li><a href="#">VTRS</a></li>
+          <li><a href="#">Embarcações</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Registro de Ocorrências</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+     
+      <li><a href=" http://mytestsite-com-br.umbler.net/logout.php "><span class="glyphicon glyphicon-log-in"></span> Sair</a></li>
+    </ul>
+  </div>
+</nav>
+<!---->
+
+
+
+<div class="row">				<div class="col-sm-6">					<h2>Clientes</h2>				</div>				<div class="col-sm-6 text-right h2">			    	<a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Cliente</a>			    	<a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>			    </div>			</div>
+</head>
+
+<body>
+
 <!--conec -->
 oi
 </body>

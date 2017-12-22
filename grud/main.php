7ea9e7604aca26e809f0b8cdc9a1e61dd2ed1410
joaoaluz <joao_aluz@hotmail.com>
2017-12-21 23:32:36 -0200

    s

--- grud/main.php
+++ grud/main.php
@@ -56,9 +56,40 @@ require "/home/mytestsite.com.br/public/grud/function.php";
 <body>
 
 
-<?php if (!empty($_SESSION['message'])) : ?>			<div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>				<?php echo $_SESSION['message']; ?>			</div>			<?php clear_messages(); ?>		<?php endif; ?>		
+<?php if (!empty($_SESSION['message'])) : ?>			
+	<div class="alert alert-<?php echo $_SESSION['type']; ?> alert-dismissible" role="alert">				
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>				
+		<?php echo $_SESSION['message']; ?>			
+	</div>			
+	<?php clear_messages(); ?>		<?php endif; ?>		
 <hr>		
-<table class="table table-hover">		<thead>			<tr>				<th>ID</th>				<th width="30%">Nome</th>				<th>CPF/CNPJ</th>				<th>Telefone</th>				<th>Atualizado em</th>				<th>Opções</th>			</tr>		</thead>		<tbody>		<?php if ($MaterialOp) : ?>		<?php foreach ($MaterialOp as $customer) : ?>			<tr>				<td><?php echo $customer['id']; ?></td>				<td><?php echo $customer['name']; ?></td>				<td><?php echo $customer['cpf_cnpj']; ?></td>				<td>00 0000-0000</td>				<td><?php echo $customer['modified']; ?></td>				<td class="actions text-right">					<a href="view.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>					<a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i> Editar</a>					<a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal" data-customer="<?php echo $customer['id']; ?>">						<i class="fa fa-trash"></i> Excluir					</a>				</td>			</tr>		<?php endforeach; ?>		<?php else : ?>			<tr>				<td colspan="6">Nenhum registro encontrado.</td>			</tr>		<?php endif; ?>		</tbody>		</table>
+<table class="table table-hover">		
+	<thead>			
+		<tr>				
+			<th>ID</th>				
+			<th width="30%">Material</th>				
+				<th>RP</th>				
+				<th>Num-Serie</th>				
+					<th>Atualizado em</th>				
+						<th>Ocorrencia</th>			
+					</tr>		
+				</thead>		
+				<tbody>		
+					<?php if ($MaterialOp) : ?>		<?php 
+					foreach ($MaterialOp as $customer) : ?>			
+					<tr>				
+						<td><?php echo $customer['id']; ?>
+							
+						</td>				
+						<td><?php echo $customer['Material']; ?></td>
+									<td><?php echo $customer['Registro_Patrimonio']; ?></td>
+													<td>00 0000-0000</td>				
+													<td><?php echo $customer['modified']; ?></td>				
+													<td class="actions text-right">					<a href="view.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>					
+													<a href="edit.php?id=<?php echo $customer['id']; ?>" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i> Editar</a>					
+													<a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal" data-customer="<?php echo $customer['id']; ?>">						
+														<i class="fa fa-trash"></i> Excluir					</a>				</td>			</tr>		<?php endforeach; ?>		
+													<?php else : ?>			<tr>				<td colspan="6">Nenhum registro encontrado.</td>			</tr>		<?php endif; ?>		</tbody>		</table>
 
 
 

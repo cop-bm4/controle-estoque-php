cd069e4eb2951a3ac5b9b85f89cf21abfcfcfe0d
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-22 09:32:30 -0300

    php

--- grud/main.php
+++ grud/main.php
@@ -48,23 +48,25 @@
 <body>
 
 <?php
-
 $servername = "mysql785.umbler.com";
 $username = "joaoaluz";
 $password = "joao99945222";
 $db = "controle_materia"; 
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-
-
-$sql = "SELECT id, Material,  FROM MaterialOp";
+$sql = "SELECT id, Material FROM MaterialOp";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - nome: " . $row["Material"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["id"]. " - Name: " . $row["Material"]. " " . $row["lastname"]. "<br>";
     }
 } else {
     echo "0 results";

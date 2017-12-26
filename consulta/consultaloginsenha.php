ada6327adc327751e555992bc3c0fb3d8ab3acdb
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 08:27:22 -0300

    s

--- consulta/consultaloginsenha.php
+++ consulta/consultaloginsenha.php
@@ -4,7 +4,6 @@
 	<title></title>
 </head>
 <body>
-<!--
 <?php
 $servername = "mysql785.umbler.com";
 $username = "joaoaluz";
@@ -31,48 +30,7 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
--->
 
-<?php
-// Conexão ao banco
-
-$link = mysql_connect('mysql785.umbler.com','joaoaluz','joao99945222');
-
-// Seleciona o Banco de dados através da conexão acima
-
-$conexao = mysql_select_db('controle_materia',$link); if($conexao){
-
-$sql = "SELECT nome FROM usuarios";
-
-$consulta = mysql_query($sql);
-
-echo '<table>';
-
-echo '<tr>';
-
-echo '<td>Nome</td>';
-
-
-
-echo '</tr>';
-
-// Armazena os dados da consulta em um array associativo
-
-while($registro = mysql_fetch_assoc($consulta)){
-
-echo '<tr>';
-
-echo '<td>'.$registro["nome"].'</td>';
-
-echo '</tr>';
-
-}
-
-echo '</table>';
-
-}
-
-?>
 </body>
 </html>
 

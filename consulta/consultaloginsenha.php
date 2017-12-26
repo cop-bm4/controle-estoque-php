857abff4551d346894c9b9bbce2a24c97c3eab20
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 09:02:25 -0300

    login

--- consulta/consultaloginsenha.php
+++ consulta/consultaloginsenha.php
@@ -4,34 +4,25 @@
 	<title></title>
 </head>
 <body>
-
-oi
-
 <?php
-
 $servername = "mysql785.umbler.com";
 $username = "joaoaluz";
 $password = "joao99945222";
 $db = "controle_materia"; 
-
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+$conn = new mysqli($servername, $username, $password, $db);
+
+$sql = "SELECT login, senha FROM usuarios Where `login` = '$login' AND `senha`= '$senha'";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows ($result) > 0 ){
+echo .$row['login'].;
+
+
 }
 
-
-$result = mysqli_query("SELECT id,nome FROM usuarios WHERE id = '1'");
-if (!$result) {
-    echo 'Could not run query: ' . mysql_error();
-    exit;
-}
-$row = mysqli_fetch_row($result);
-
-echo $row[0]; // 42
-echo $row[1]; // the email value
 ?>
+
 
 
 </body>

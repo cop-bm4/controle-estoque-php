81eb0e6b092cdf43eea156c1c749a21553ca39b3
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 09:08:53 -0300

    ç

--- consulta/consultaloginsenha.php
+++ consulta/consultaloginsenha.php
@@ -4,6 +4,7 @@
 	<title></title>
 </head>
 <body>
+
 <?php
 $servername = "mysql785.umbler.com";
 $username = "joaoaluz";
@@ -12,7 +13,7 @@ $db = "controle_materia";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
 
-$sql = "SELECT login, senha FROM usuarios Where `login` = '$login' AND `senha`= '$senha'";
+$sql = "SELECT login, senha FROM usuarios ";
 $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows ($result) > 0 ){

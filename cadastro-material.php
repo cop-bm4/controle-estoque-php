fde2a4cb94d540d119c7cee264dd0f8def63fba3
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 11:15:27 -0300

    s

--- cadastro-material.php
+++ cadastro-material.php
@@ -81,10 +81,8 @@ echo "Não foi possível conectar ao banco de dados";
   
 </div>
 
-<div class="row">
-    <div class="col-sm-6">
-      <h2>Clientes</h2>
-    </div>
+<div class="container">
+   
     <div class="col-sm-6 text-right h2">
         <a class="btn btn-primary" href="add.php"><i class="fa fa-plus"></i> Novo Material</a>
         <a class="btn btn-default" href="http://mytestsite-com-br.umbler.net/cadastro-material.php"><i class="fa fa-refresh"></i> Atualizar</a>

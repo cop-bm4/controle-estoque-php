816022e3c1baf5e4f975917705c2dca56223a4f1
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 11:35:02 -0300

    cadstronovo

--- cadastro.php
+++ cadastro.php
@@ -1,35 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>cadastro material operacional</title>
+</head>
+<body>
 
-<?php
-$nome = $_POTS ['nome']
-$login = $_POST['login'];
-$senha = MD5($_POST['senha']);
-$connect = mysql_connect('mysql785.umbler.com','joaoaluz','joao99945222');
-$db = mysql_select_db('controle_materia');
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
-$logarray = $array['login'];
- 
-  if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
- 
-    }else{
-      if($logarray == $login){
- 
-        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastro.html';</script>";
-        die();
- 
-      }else{
-        $query = "INSERT INTO usuarios (nome,login,senha) VALUES ('$nome',$login','$senha')";
-        $insert = mysql_query($query,$connect);
-         
-        if($insert){
-          echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
-        }else{
-          echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>";
-        }
-      }
-    }
-
-
- ?>
+</body>
+</html>
---
+++ cadastronovo.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>cadastro material operacional</title>
+
+    <!--login -->
+<?php  
+session_start();
+if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
+{
+  unset($_SESSION['login']);
+  unset($_SESSION['senha']);
+  header('location:home.php');
+  }
+
+$logado = $_SESSION['login'];
+?>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!---->
+  <!-- include bibliotecas bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <?php
+  require "db.php"; 
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+echo "Não foi possível conectar ao banco de dados";
+} 
+
+
+?>
+<!--conec -->
+
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"> SISCOMP </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li ><a href="http://mytestsite-com-br.umbler.net/home.php">Home</a></li>
+      <li class= "active" class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="http://mytestsite-com-br.umbler.net/cadastro-material.php">Registro<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="http://mytestsite-com-br.umbler.net/cadastro-material.php" >Material</a></li>
+          <li><a href="#">VTRS</a></li>
+          <li><a href="#">Embarcações</a></li>
+        </ul>
+      </li>
+      
+ <li><a href="#">Relatórios</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+     
+      <li><a href=" http://mytestsite-com-br.umbler.net/logout.php "><span class="glyphicon glyphicon-log-in"></span> Sair</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+<div class="container">
+<div class="jumbotron">
+<h1>SISCOMP</h1> 
+    <p class="text-danger">Cadastro de Material Operacional </p> 
+  </div>
+  
+</div>
+
+</body>
+</html>

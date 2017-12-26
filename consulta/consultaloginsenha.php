0845f6c6df354c66f569faa653be0c134cf792c3
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 09:16:00 -0300

    w

--- consulta/consultaloginsenha.php
+++ consulta/consultaloginsenha.php
@@ -18,7 +18,7 @@ $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows ($result) > 0 ){
 
- while($row = $result->fetch_assoc()) {
+ while($row = mysqli_fetch_assoc($result)) {
         echo "id: " . $row["id"]. " - Name: " . $row["nome"].  "<br>";
     }
 

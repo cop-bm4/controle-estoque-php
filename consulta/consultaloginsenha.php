01084b26acf12fca42466a2d7840f913892ea6c1
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 10:27:36 -0300

    s

--- consulta/consultaloginsenha.php
+++ consulta/consultaloginsenha.php
@@ -20,11 +20,56 @@ if(mysqli_num_rows ($result) > 0 ){
 
  while($row = mysqli_fetch_assoc($result)) {
         echo "id: " . $row["id"]. " - Name: " . $row["Nome"]. "Material" .$row["material"]. "Numero de Serie:" .$row["nserie"]. "Ocorrencia" .$row["ocorrencia"]. "rp:" .$row["rp"].  "<br>";
+   
+<table class="table table-hover">
+<thead>
+	<tr>
+		<th>ID</th>
+		<th width="30%">Nome</th>
+		<th>CPF/CNPJ</th>
+		<th>Telefone</th>
+		<th>Atualizado em</th>
+		<th>Opções</th>
+	</tr>
+</thead>
+<tbody>
+	<tr>
+		<td><?php echo $row['id']; ?></td>
+		<td><?php echo $row['name']; ?></td>
+		<td><?php echo $row['cpf_cnpj']; ?></td>
+		<td>00 0000-0000</td>
+		<td><?php echo $row['modified']; ?></td>
+		<td class="actions text-right">
+			<a href="view.php?id=<?php echo $row['id']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>
+			<a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i> Editar</a>
+			<a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal" data-customer="<?php echo $customer['id']; ?>">
+				<i class="fa fa-trash"></i> Excluir
+			</a>
+		</td>
+	</tr>
+
+	
+
+</tbody>
+</table>
+
+
+
+
+
+
     }
 
 
 
 }
+
+
+
+
+
+
+
 
 ?>
 

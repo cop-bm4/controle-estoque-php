cf388287ca89a58c322a4063eb3c78ee7b826bd1
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 12:51:41 -0300

    rr

--- cadastronovo.php
+++ cadastronovo.php
@@ -84,7 +84,7 @@ if ($conn->connect_error) {
 } 
 $stmt = $conn->prepare("INSERT INTO  materialoperacional(ubm, material, tipo, rp, nserie, situacao, ocorrencia, nome, rg) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-$stmt->bind_param("sss",$ubm, $material, $tipo, $rp, $nserie, $situacao, $ocorrencia, $nome, $rg);
+$stmt->bind_param($ubm, $material, $tipo, $rp, $nserie, $situacao, $ocorrencia, $nome, $rg);
 
 $ubm = "30 gbm";
 $material = "capa";

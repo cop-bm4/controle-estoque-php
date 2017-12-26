d6598aa02204ab889eceb7c4462d11e43910403d
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2017-12-26 08:14:17 -0300

    s

--- consulta/consultaloginsenha.php
+++ consulta/consultaloginsenha.php
@@ -20,7 +20,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-         echo "id: " . $row["id"]. " - Name: " . $row["nome"]. " login: " . $row["login"]. "senha: ".$row["senha"]. "<br>";
+         echo "id: " . $row["id"].  "<br>";
     }
 } else {
     echo "0 results";

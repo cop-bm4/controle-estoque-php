33d262bc64927095c2650cf1848d391a408439c4
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-02 12:44:18 -0300

    s

--- cadastronovo.php
+++ cadastronovo.php
@@ -13,10 +13,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-  <?php
+ <?php
   require "db.php"; 
 
 // Check connection
@@ -54,6 +51,9 @@ echo "Não foi possível conectar ao banco de dados";
   </div>
 </nav>
 
+</head>
+<body>
+ 
 
 <form action="cadastro.php" method="post">
   <div class="container">

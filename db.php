d4eacb58adc657a61c163594d20dbf096dc6322c
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-02 09:01:48 -0300

    mudanca de bd

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "mysql785.umbler.com";
-$username = "joaoaluz";
+$username = "joaoluz";
 $password = "joao99945222";
-$db = "controle_materia"; 
+$db = "controlematerial"; 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
 

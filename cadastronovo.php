ffab102f4ee4b07f13b65de05ab56246b37e9e60
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-03 09:46:14 -0300

    z

--- cadastronovo.php
+++ cadastronovo.php
@@ -64,6 +64,7 @@
           </div>
           
           <div>
+            
             <div class="input-group mb-3">
               <div class="input-group-prepend">
                 <label class="input-group-text" for="selec">Material</label>
@@ -75,6 +76,17 @@
               </select>
 
                </div> 
+               <div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <label class="input-group-text" for="inputGroupSelect01">Options</label>
+  </div>
+  <select class="custom-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+</div>
 
 
           </div>

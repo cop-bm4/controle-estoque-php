543cea78ffba8a29a4b98b270917748d952e3e39
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-03 09:05:12 -0300

    hotdor

--- cadastronovo.php
+++ cadastronovo.php
@@ -66,7 +66,7 @@
   
 
 <select class="form-control" data-live-search="true" name="teste">
-  <option data-tokens="ketchup mustard" value="hot Dog">Hot Dog, Fries and a Soda</option>
+  <option data-tokens="hotdog" value="hot Dog">Hot Dog, Fries and a Soda</option>
   <option data-tokens="mustard" value="Burger">Burger, Shake and a Smile</option>
   <option data-tokens="frosting" value="Sugar">Sugar, Spice and all things nice</option>
 </select>

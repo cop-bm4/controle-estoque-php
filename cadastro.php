4670acee0e80eb8c2d2c89b01faa29601310ee59
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-03 10:30:54 -0300

    situcao

--- cadastro.php
+++ cadastro.php
@@ -7,12 +7,13 @@
 $material = $_POST['material'];
 $rp = $_POST['rp'];
 $nserie = $_POST['nserie'];
+$situacao = $_POST['situacao']
 ?>
 <?php
 
 
-$sql = "INSERT INTO materialoperacional (material, rp, nserie)
-VALUES ('$material', '$rp', 'nserie')";
+$sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao)
+VALUES ('$material', '$rp', '$nserie', '$situacao')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";

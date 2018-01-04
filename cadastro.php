43050251cbfe82168ec51de3b2d1bffb2d7f5acb
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-04 10:15:51 -0300

    SS

--- cadastro.php
+++ cadastro.php
@@ -3,24 +3,21 @@
 <?php require 'db.php';  ?>
 <?php require 'credenciais.php' ?>
 <?php
-$login = $_POST['login'];
-$senha = $_POST['senha'];
+
 
 $material = $_POST['material'];
 $rp = $_POST['rp'];
 $nserie = $_POST['nserie'];
 $situacao = $_POST['situacao'];
 $ocorrencia = $_POST['ocorrencia']; 
-$ubm = $_POST['ubm'];
+$ubm = $_SESSION['ubm'];
 $nome = $_POST['nome'];
 $rg = $_POST['rg'];
+
 ?>
 <?php
 
-$sql1 = "SELECT login, senha, ubm FROM materialoperacional Where `login` = '$login' AND `senha`= '$senha'";
-$result = mysqli_query($conn, $sql1);
-$tabela = mysqli_fetch_assoc($result);
-$ub = $result['ubm']; 
+
 
 $sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia, ubm, nome, rg)
 VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia','$ub','$nome','$rg')";
--- validalogin.php
+++ validalogin.php
@@ -16,9 +16,11 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 
-$sql = "SELECT login, senha FROM materialoperacional Where `login` = '$login' AND `senha`= '$senha'";
+$sql = "SELECT login, senha, ubm FROM materialoperacional Where `login` = '$login' AND `senha`= '$senha'";
 $result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
 
+$ubm = $row['ubm'];
 // A variavel $result pega as varias $login e $senha, faz uma pesquisa na tabela de usuarios
 
 /* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do resultado ele redirecionará para a pagina site.php ou retornara  para a pagina do formulário inicial para que se possa tentar novamente realizar o login */
@@ -27,7 +29,7 @@ if(mysqli_num_rows ($result) > 0 )
 {
 $_SESSION['login'] = $login;
 $_SESSION['senha'] = $senha;
-
+$_SESSION['ubm']   = $ubm;
 header('location:home.php');
 echo "deu certo";
 }

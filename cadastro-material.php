77dcb30e40e1235817bb5be495a20a130195c4be
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-04 12:18:09 -0300

    s

--- cadastro-material.php
+++ cadastro-material.php
@@ -73,7 +73,7 @@ echo "Não foi possível conectar ao banco de dados";
   </div>
 
 <?php
-$sql = "SELECT id, ubm, material, rp, nserie, situacao, ocorrencia, nome, rg, reg_date FROM materialoperacional WHERE 'ubm'='$ubm'";
+$sql = "SELECT id, ubm, material, rp, nserie, situacao, ocorrencia, nome, rg, reg_date FROM materialoperacional WHERE 'ubm' =' $ubm' ";
 $result = mysqli_query($conn, $sql);
 ?>
 

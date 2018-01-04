a666838990b11f880b3c99fd76eca7203d0cb1db
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-04 08:18:14 -0300

    teste final pra

--- cadastro.php
+++ cadastro.php
@@ -9,12 +9,13 @@ $rp = $_POST['rp'];
 $nserie = $_POST['nserie'];
 $situacao = $_POST['situacao'];
 $ocorrencia = $_POST['ocorrencia']; 
+$ubm = $_POST['ubm'];
 ?>
 <?php
 
 
-$sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia)
-VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia')";
+$sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia, ubm)
+VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia','$ubm')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";

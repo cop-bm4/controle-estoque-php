95aab79f744abeb433fe0c9b0a0b965bfc4a02cd
joaoaluz <joao_aluz@hotmail.com>
2018-01-05 20:12:48 -0200

    sem id

--- view.php
+++ view.php
@@ -51,7 +51,7 @@ require 'db.php';
 
 <?php 
 $id = $_GET['id'];
-echo "id .$id. ";
+
 
 ?>
 
@@ -80,7 +80,7 @@ echo $row['cargo'];
  
  	 
      
-  <h1>Visualizaçâo Detalhada do Material</h1>    
+  <h2>Visualizaçâo Detalhada do Material</h2>    
   <br>
   <p class="lead"> <strong> Material: </strong> <?php echo $row['material']; ?> <strong> R. Patrimônial: </strong> <?php echo $row['rp']; ?> <strong> N. Série: </strong> <?php echo $row['nserie'];?>  </p>
 

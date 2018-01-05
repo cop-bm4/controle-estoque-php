404b21185c7d6509b5cc59cbf395f7616e7caed5
joaoaluz <joao_aluz@hotmail.com>
2018-01-05 19:51:52 -0200

    2

--- view.php
+++ view.php
@@ -80,7 +80,7 @@ echo $row['cargo'];
  	 
      <dt class="col-sm-1"> Material: </dt>
      <dd class="col-sm-1"><?php echo $row['material']; ?></dd>
-     <dt class="col-sm-1">R. Patrimônial: </dt>
+     <dt class="col-sm-2">R. Patrimônial: </dt>
      <dd class="col-sm-1"><?php echo $row['rp'] ?></dd>
    
    </dl>

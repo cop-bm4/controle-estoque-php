7e3e63551a616da09b76241203dc253654f72a66
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-05 08:00:12 -0300

    crede

--- view.php
+++ view.php
@@ -1,11 +1,24 @@
 
-
+<!DOCTYPE html>
+<html>
+<head>
 
 <?php
-
+require 'credenciais.php';
 require 'db.php';
 
 $id = $_GET['id'];
 echo "deu bom";
 echo $id;
 ?>
+	<title>Visualizar</title>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
+
+

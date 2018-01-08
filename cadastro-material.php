124a23ea583b7d2b8ae3bc9e7b11a92ac4a85c9d
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-08 12:05:53 -0300

    lixo

--- cadastro-material.php
+++ cadastro-material.php
@@ -110,9 +110,8 @@ $result = mysqli_query($conn, $sql);
     <td class="actions text-right">
       <a href="http://copbm4-com-br.umbler.net/view.php?id=<?php echo $row['id']; ?>"button type="button" class="btn btn-default"><i class="fa fa-eye"></i> Visualizar</a>
       <a href="edit.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default"><i class="fa fa-pencil"></i> Editar</a>
-      <a href="http://copbm4-com-br.umbler.net/excluir.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default" data-toggle="modal" data-target="#delete-modal" data-customer="<?php echo $row['id']; ?>">
-        <i class="fa fa-trash"></i> Excluir
-      </a>
+      <a href="http://copbm4-com-br.umbler.net/excluir.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default" >
+        <i class="fa fa-trash"></i> Excluir</a>
     </td>
   </tr>
 

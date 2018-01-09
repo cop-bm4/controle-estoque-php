d9e84b365c67c3d0112f153ef269c6df1d247a65
joaoaluz <joao_aluz@hotmail.com>
2018-01-08 23:39:33 -0200

    s

--- excluir.php
+++ excluir.php
@@ -115,11 +115,11 @@ $row = mysqli_fetch_assoc($result);
 	<p class="lead" align="center">VOCÊ RELAMENTE DESEJA EXCLUIR O ITEM ACIMA ?</p>
 	
   <button type="submit" class="btn btn-danger btn-block" value="sim" name="butao">Sim </button>
-  
+  <button href="http://copbm4-com-br.umbler.net/cadastro-material.php" type="button" class="btn btn-secondary btn-block"  >Não</button>
 </div>
 
 </form>
-<button type="button" class="btn btn-secondary btn-block" value="nao" href="http://copbm4-com-br.umbler.net/cadastro-material.php">Não</button>
+
 <?php 
 
 $aux = $_POST['butao'];

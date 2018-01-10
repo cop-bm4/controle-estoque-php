c93b5c18f36a8d14e92512eecf7fcca4bb1ced40
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-10 09:23:42 -0300

    edit

--- edit.php
+++ edit.php
@@ -24,7 +24,7 @@ echo $situacao;
 
 */
 
-$sql = "UPDATE 'materialoperacional SET'  rp='123' nome='joao' WHERE id='58' ";
+$sql = "UPDATE materialoperacional SET  'rp'='123' 'nome'='joao' WHERE 'id'='58' ";
 
 
 if ($conn->query($sql) === TRUE) {

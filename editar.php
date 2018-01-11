0d201e0a15b034c2c164fe6ab789b4aee06820f3
joaoaluz <joao_aluz@hotmail.com>
2018-01-10 22:41:37 -0200

    s

--- editar.php
+++ editar.php
@@ -243,50 +243,28 @@ $row = mysqli_fetch_assoc($result);
    </div>
 </form>
 
-
-
 <?php
 
  if(isset($_POST['rp'])){
 $material = $_POST['material'];
-$rp = $_POST['rp'];
-$nserie = $_POST['nserie'];
-$situacao = $_POST['situacao'];
-$ocorrencia = $_POST['ocorrencia']; 
-
-$id = $_POST['id'];
-
-$nome = $_POST['nome'];
-
-$rg = $_POST['rg'];
-$cargo = $_POST['cargo'];
-$defeito = $_POST['defeito'];
-
-
-
-
-$sql = "UPDATE materialoperacional SET nome='$nome' rg='$rg' rp='$rp' nserie='$nserie' situacao='$situacao' ocorrencia='$ocorrencia' cargo='$cargo' defeito='defeito' WHERE id='$id' ";
-
+echo $material;
+echo "nome";
+$sql = " UPDATE materialoperacional SET material='$material'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Atualização feita com sucesso";
-  
+    
     
 } else {
     echo "Error updating record: " . $conn->error;
 }
 
 $conn->close();
-
-
-}
-
-  
+  }
 
 
 
 ?>
-
 
  <div class="footer">
   <p>© 2018 BM4/COP  CBM-PA V.C João Luz </p>

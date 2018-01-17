5e4a7789cbf8d064b85e6313f9c570f42710dc0b
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-17 11:01:26 -0300

    mastereditar

---
+++ masteredit.php
@@ -0,0 +1,39 @@
+<?php 
+    require 'db.php';
+	require 'credenciais.php';
+ 
+
+
+
+$material = $_POST['material'];
+$rp = $_POST['rp'];
+$nserie = $_POST['nserie'];
+$situacao = $_POST['situacao'];
+$ocorrencia = $_POST['ocorrencia']; 
+
+$id = $_POST['id'];
+$marca= $_POST['marca'];
+$nome = $_POST['nome'];
+
+$rg = $_POST['rg'];
+$cargo = $_POST['cargo'];
+$defeito = $_POST['defeito'];
+
+
+
+
+$sql = "UPDATE materialoperacional SET nome='$nome', rg='$rg', rp='$rp', nserie='$nserie', situacao='$situacao', ocorrencia='$ocorrencia', cargo='$cargo', defeito='$defeito', marca='$marca' WHERE id='$id' ";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "Atualização feita com sucesso";
+    header("location:mastercontrole-material.php");
+    
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+
+
+?>

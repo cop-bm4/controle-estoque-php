9343b4a5752d40ddd1aad8bc79dac036c9146c6b
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-17 10:21:45 -0300

    ere

--- validalogin.php
+++ validalogin.php
@@ -19,20 +19,28 @@ echo "Connected successfully";
 $sql = "SELECT login, senha, ubm FROM materialoperacional Where `login` = '$login' AND `senha`= '$senha'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
-
+$lg= $row['login'];
 $ubm = $row['ubm'];
 $_SESSION['ubm']   = $ubm;
 // A variavel $result pega as varias $login e $senha, faz uma pesquisa na tabela de usuarios
 
 /* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do resultado ele redirecionará para a pagina site.php ou retornara  para a pagina do formulário inicial para que se possa tentar novamente realizar o login */
 
-if(mysqli_num_rows ($result) > 0 )
+if(mysqli_num_rows ($result) > 0 and $lg!='master' )
 {
 $_SESSION['login'] = $login;
 $_SESSION['senha'] = $senha;
 $_SESSION['ubm']   = $ubm;
 header('location:home.php'); 
-echo "deu certo ";
+
+
+}
+elseif (myssli_num_rows($result)>0 and $lg='master' {
+	$_SESSION['login'] = $login;
+    $_SESSION['senha'] = $senha;
+    $_SESSION['ubm']   = $ubm;
+    header('location:homemaster.php'); 
+
 
 }
 else{

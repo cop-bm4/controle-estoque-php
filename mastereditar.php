493a030a6205c5fd7fcb64ae343ae991c519e6b8
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-17 12:42:56 -0300

    marce

--- mastereditar.php
+++ mastereditar.php
@@ -124,7 +124,7 @@ $row = mysqli_fetch_assoc($result);
        <div class="col-lg-6">
          <div class="input-group">
            <span class="input-group-addon">Marca</span>
-           <input type="text" name="marca" value="<?php echo $row['marca'] ?>" >
+           <input type="text"  class="form-control" arial-label="Default" aria-describelby="inputGroup-sizing-default" name="marca" value="<?php echo $row['marca'] ?>" disabled>
          </div>
    
        </div> 

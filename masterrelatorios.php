53e1732065a3d1944894f67f00c7711f18bf9c51
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-18 12:18:47 -0300

    SSSSS

--- masterrelatorios.php
+++ masterrelatorios.php
@@ -270,12 +270,39 @@
             <div class="input-group">
               <span class="input-group-addon">UBM</span>
               <select class="form-control" data-live-search="true" name="ub">
-                <option data-tokens="qualquer" value="qualquer" >Qualquer</option> 
-                <option data-tokens="1 gbm" value="1 GBM">1º GBM </option>
-                <option data-tokens="3 gbm" value="3 GBM">3º GBM</option>
-                 <option data-tokens="3 gbm" value="4 GBM">4º GBM</option>
-                <option data-tokens="30 gbm" value="30 GBM"> 30º GBM</option>
-                
+                <option data-tokens="1 GBM" value="1 GBM"> 1 GBM </option>
+<option data-tokens="2 GBM" value="2 GBM">  2 GBM </option>
+<option data-tokens="3 GBM" value="3 GBM">  3 GBM </option>
+<option data-tokens="4 GBM" value="4 GBM">  4 GBM </option>
+<option data-tokens="5 GBM" value="5 GBM">  5 GBM </option>
+<option data-tokens="6 GBM" value="6 GBM">  6 GBM </option>
+<option data-tokens="7 GBM" value="7 GBM">  7 GBM </option>
+<option data-tokens="8 GBM" value="8 GBM">  8 GBM </option>
+<option data-tokens="9 GBM" value="9 GBM">  9 GBM </option>
+<option data-tokens="10 GBM" value="10 GBM">  10 GBM  </option>
+<option data-tokens="11 GBM" value="11 GBM">  11 GBM  </option>
+<option data-tokens="12 GBM" value="12 GBM">  12 GBM  </option>
+<option data-tokens="13 GBM" value="13 GBM">  13 GBM  </option>
+<option data-tokens="14 GBM" value="14 GBM">  14 GBM  </option>
+<option data-tokens="15 GBM" value="15 GBM">  15 GBM  </option>
+<option data-tokens="16 GBM" value="16 GBM">  16 GBM  </option>
+<option data-tokens="17 GBM" value="17 GBM">  17 GBM  </option>
+<option data-tokens="18 GBM" value="18 GBM">  18 GBM  </option>
+<option data-tokens="19 GBM" value="19 GBM">  19 GBM  </option>
+<option data-tokens="20 GBM" value="20 GBM">  20 GBM  </option>
+<option data-tokens="21 GBM" value="21 GBM">  21 GBM  </option>
+<option data-tokens="22 GBM" value="22 GBM">  22 GBM  </option>
+<option data-tokens="23 GBM" value="23 GBM">  23 GBM  </option>
+<option data-tokens="24 GBM" value="24 GBM">  24 GBM  </option>
+<option data-tokens="25 GBM" value="25 GBM">  25 GBM  </option>
+<option data-tokens="26 GBM" value="26 GBM">  26 GBM  </option>
+<option data-tokens="27 GBM" value="27 GBM">  27 GBM  </option>
+<option data-tokens="28 GBM" value="28 GBM">  28 GBM  </option>
+<option data-tokens="29 GBM" value="29 GBM">  29 GBM  </option>
+<option data-tokens="30 GBM" value="30 GBM">  30 GBM  </option>
+<option data-tokens="1 GPA" value="1 GPA">  1 GPA </option>
+<option data-tokens="1 GBS" value="1 GBS">  1 GBS </option>
+<option data-tokens="1 GMAF" value="1 GMAF">  1 GMAF  </option>
               </select>
             </div>
             <p id="passwordHelpBlock" class="form-text text-muted">
@@ -295,7 +322,7 @@
           $nomematerial = array("ABAFADOR INCENDIO", "AFT", "ALAVANCA", "APARELHO OITO", "APARELHO PA", "APARELHO CORTA A FRIO", "ANEL DE FITA", "APARELHO TIFFOR", "ALICATE UNIVERSAL", "ALICATE  DE PRESSAO", "ANILHA", "APARELHO CROCK", "APARELHO EXTINTOR INCÊNDIO", "APARELHO GPS", "APARELHO GERADOR ESPUMA", "ADAPTADOR FEMEA", "ADAPTADOR MACHO", "ASCENSOR DESCENSOR", "BOIA SALVA VIDAS", "BALACLAVA", "BANDEIRA GV", "BARRACA GV (tenda)", "BARRACA CEDEC", "BLOCANTE", "BINÓCULO", "BOTA DE INCÊNDIO", "BOTA  CANO LONGO", "BOMBA FLUTUANTE", "BOMBA SUBMERSA", "BOMBA REBOQUE", "BOMBAPORTATIL", "BOLSA DE APH", "BOMBA COSTAL PARA FOGO NO MATO", "BOTE INFLAVEL", "CAIXA TERMICA", "CABO DE ACO", "CAMBAO", "CHAVE AMERICANA", "CHAVE DE MANGUEIRA", "CHAVE DE HIDRANTE", "CHAVE DE MANGOTE", "CHAVE DE MUNHAO", "CHAVE DE RODA", "CHAVE DE VELA", "CHAVE DE ELEVADOR", "CONE DE SINALIZACAO", "CORDA DE SALVAMENTO", "CORDA DE NYLON", "CORDA COMUM", "CONJUNTO DE APROXIMACAO (CALÇA / CAPA)", "CONJUNTO FRESS SEGER", "CAPACETE SALVAMENTO", "CAPACETE INCENDIO", "CAPACETE INCENDIO FLOR", "CAPA DE CHUVA", "CANTIL", "CINTO SALVAMEN", "CADEIRINHA SALVAMENTO", "CADEIRAO  PRAIA", "CILINDRO PA", "COLETE DE SINALIZACAO", "COLETE SALVA  VIDAS", "COLETOR", "COMPRESSOR DE AR", "DERIVANTE", "DRAGA MANUAL", "DIVISOR TRES SAIDAS", "DIVISAO DE DUAS SAIDAS", "DESENCARCERADOR A OLEO", "DESENCARCERADOR ELETRICO", "ESCADA DE MADEIRA", "ESCADA EM  FIBRA", "ESCADA EM ALUMINIO", "ESCADA DE CORDA", "ESCALER", "ESGUICHO  2 ½”", "ESGUICHO 1 ½”", "ESGUICHO CANHAO", "ESGUICHO NPU", "ESGUICHO AGULHETA", "ESGUICHO PROPORCIONADOR DE ESPUMA 2 ½”", "ESGUICHO PPROPORCIONADOR DE ESPUMA 1 ½”", "ENXADA", "ENXADECO", "EXPANSOR", "FOICE", "FACAO OUTERCADO", "FERRO DE COVA", "FITA DE ANCORAGEM", "FLUTSPUMA (FLUTUADOR DE SALVAMENTO AQUÁTICO)", "GAIOLA", "GADANHO", "GARFO", "GARATÉIA", "GUARDA-SOL", "HOLOFOTE SIMPLES", "HOLOFOTE COM TRIPÉ", "HOSPITAL CAMPANHA", "LANTERNA RECARREGÁVEL", "LINGA DE ACO", "LUVA ALTA TENSAO", "LUVA RASPA DE COURO", "LUVA DE VAQUETA", "LUVA DE SALVAMENTO", "LUVA DE INCENDIO", "MACA PVC (cesto)", "MACA LONA", "MACA COM RODAS", "MASCARA CONTRA GASES", "MASCARA PANORAMICA", "MACACO MECANICO", "MACHADO LENHADOR", "MACHADO BOMBEIRO", "MANGUEIRA 1 ½”", "MANGUEIRA 2 ½”", "MANGOTE 4”", "MACHADINHA BOMBEIRO", "MALHO", "MARTELO UNIVERSAL", "MICRO RETIFICA", "MOLA MOSQUETAO", "MOTOSERRA", "MOTO ESMERIL", "MOTO  SERRA SABRE", "MOTO BOMBA DIESEL", "MOTO BOMBAELETRICA", "MOTO GERADOR", "MANILHA", "MOTOR DE P  15 HP", "MOTOR DE P  25 HP", "MOTOR DE POPA 40 HP", "MOTOR DE POPA 90 HP", "MOTOR DE POPA 115 HP", "MISTURADOR ENTRE LINHAS", "NADADEIRA", "OCULOS PROTEÇÃO", "OXÍMETRO DE PULSO", "PINO PARA CHOQUE", "PE DE CABRA", "PA DE BICO", "PA QUADRADA", "PRANCHA SALVAMENTO", "PROTETOR AURICULAR", "PICARETA", "PINGA FOGO", "PLACA DE ANCORAGEM", "RALO DE MANGOTE", "RADIO HT ANALOGICO", "RADIO HT DIGITAL", "RADIO FIXO", "RADIO VTR", "ROUPA APICULTOR", "ROUPA MERGULHO", "ROUPA SANEAMENTO  C/ BOTA", "REDUCAO", "ROLDANA SIMPLES", "ROLDANA DUPLA", "SIRENE INTERNA", "SUPORTE BAK PAK", "TENDA GV", "TÁBUA DE SALVAMENTO", "TALHA", "TALABARTE", "TORRE DE ILUMINACAO", "TRIPE RESG POÇO", "VALVULA MERGULHO","TESADO","MCLEOD");
             $tamanhonomematerial= count($nomematerial);
 
-            $ubms = array("1 GBM","2 GBM","3 GBM", "4 GBM","5 GBM","6 GBM", "30 GBM");
+            $ubms = array("1 GBM", "2 GBM", "3 GBM", "4 GBM", "5 GBM", "6 GBM", "7 GBM", "8 GBM", "9 GBM", "10 GBM", "11 GBM", "12 GBM", "13 GBM", "14 GBM", "15 GBM", "16 GBM", "17 GBM", "18 GBM", "19 GBM", "20 GBM", "21 GBM", "22 GBM", "23 GBM", "24 GBM", "25 GBM", "26 GBM", "27 GBM", "28 GBM", "29 GBM", "30 GBM", "1 GPA", "1 GBS", "1 GMAF");
             $tamanhoubms= count($ubms);
       ?>
 

5eb838df5dfbcf2675d5091300a6076a0ec5f465
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-22 10:35:56 -0300

    s

--- cadastro.php
+++ cadastro.php
@@ -18,14 +18,21 @@ $rg = $_POST['rg'];
 $cargo = $_POST['cargo'];
 $defeito = $_POST['defeito'];
 
-
+$prefixo=$_POST['prefixo'];
+$placa=$_POST['placa'];
+$renavam=$_POST['renavam'];
+$chassi=$_POST['chassi'];
+$modelo=$_POST['modelo'];
+$origem=$_POST['origem'];
+$ano_fab=$_POST['ano_fab'];
+$lincenciamento=$_POST['lincenciamento'];
 ?>
 <?php
 
 
 
-$sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia, ubm, nome, rg, cargo, defeito, marca, observacoes)
-VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia','$ubm','$nome','$rg', '$cargo', '$defeito', '$marca', '$observacoes')";
+$sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia, ubm, nome, rg, cargo, defeito, marca, observacoes, prefixo, placa, renavam, chassi, modelo, origem, ano_fab, lincenciamento)
+VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia','$ubm','$nome','$rg', '$cargo', '$defeito', '$marca', '$observacoes', '$prefixo', '$placa','$renavam','$chassi','$chassi','$modelo','$origem','$ano_fab', '$lincenciamento')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";

661318bbbac0d6d6b9a93c308766bd5eaaba89f7
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-25 10:39:03 -0300

    3

--- cadastro.php
+++ cadastro.php
@@ -19,70 +19,22 @@ $cargo = $_POST['cargo'];
 $defeito = $_POST['defeito'];
 $observacoes= $_POST['observacoes'];
 
+$imagens= $_FILES['imagens'];
 
 
-$target_dir = "imagens/";
-$target_file = $target_dir . basename($_FILES["imagens"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["imagens"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["imagens"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["imagens"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["imagens"]["name"]). " has been uploaded.";
-        $imagens=$_FILES['imagens'];
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-
-
-
-
-
-
-/*preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $imagens["name"], $ext);
+preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $imagens["name"], $ext);
  
         	// Gera um nome único para a imagem
         	$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
  
         	// Caminho de onde ficará a imagem
-        	$caminho_imagem = "imagenss/" . $nome_imagem;
+        	$caminho_imagem = "imagens/" ;
  
 			// Faz o upload da imagem para seu respectivo caminho
 			move_uploaded_file($imagens["tmp_name"], $caminho_imagem);
 
 
-*/
+
 
 ?>
 

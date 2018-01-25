20d46570635fe9d881baa6a10d5338996cf8d0c4
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-25 12:02:19 -0300

    s

--- cadastronovo.php
+++ cadastronovo.php
@@ -79,7 +79,7 @@ require "credenciais.php";
 </div>
 
  
-<form action="cadastro.php" method="post" id="needs-validation" enctype="multipart/form-data" >
+<form action="cadastro.php" method="post" id="needs-validation" enctype="multipart/form-data">
  <div class="container">
   <div class="p-3 mb-2 bg-info text-white"> <h3>1- Cadastro Material </h3></div>
   </div>
@@ -438,10 +438,10 @@ require "credenciais.php";
 
       <div class="row">
         <div class="col-lg-6">
-         <label class="custom-file">
-              <input type="file" id="file" class="custom-file-input" name="imagens">
-                <span class="custom-file-control"></span>
-        </label>
+          <div class="input-group">
+            <span class="input-group-addon">Imagens </span>  
+            <input class="form-control" type="file" name="imagens">  
+          </div>
           <p class="form-text text-muted">Enviar foto atualizada do material.</p>
         </div>
       </div>

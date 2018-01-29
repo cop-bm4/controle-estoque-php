fb9f9bfc668d4d42d7c7e0a1975c5b31c669e957
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-29 12:15:48 -0300

    nova embarcação

--- cadastronovoembarcacoes.php
+++ cadastronovoembarcacoes.php
@@ -76,5 +76,10 @@ echo "Não foi possível conectar ao banco de dados";
   </div>
 </nav>
 
+<div class="container">
+	
+<h2 class=" text-danger " align="center"> Cadastro de Nova Embarcações</h2>
+ </div>
+
 </body>
 </html>

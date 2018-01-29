d82525a276b238379ce1b9513646eebdacddc3a9
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-29 12:04:55 -0300

    cadastro novo embarcações

---
+++ cadastronovoembarcacoes.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro de Embarcações</title>
+	 <!--login -->
+<?php 
+require "credenciais.php"
+
+
+?>
+<!--mod mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!---->
+  <!-- include bibliotecas bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!---->
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   
+   color: black;
+   text-align: center;
+   font-size: 12px; 
+}
+</style>
+
+</head>
+<body>
+
+<!-- Banco de dados conect-->
+<?php
+require "db.php"; 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+echo "Não foi possível conectar ao banco de dados";
+} 
+?>
+<!--conec -->
+
+	<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"> SISCOMP </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li ><a href="http://copbm4cbmpa-com-br.umbler.net/masterhome.php">Home</a></li>
+      <li class= "active" class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="http://copbm4cbmpa-com-br.umbler.net/mastercadastro-material.php">Registro<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="http://copbm4cbmpa-com-br.umbler.net/mastercadastro-material.php" >Material</a></li>
+          <li><a href="http://copbm4cbmpa-com-br.umbler.net/mastercadastro-vtrs.php">VTRS</a></li>
+                  <li><a href="http://copbm4cbmpa-com-br.umbler.net/mastercadastro-embarcacoes.php">Embarcações</a></li>
+        </ul>
+      </li>
+      
+<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" >Relatórios</a>
+ <ul class="dropdown-menu">
+                <li><a href="http://copbm4cbmpa-com-br.umbler.net/masterrelatorios.php"> Material</a></li>
+                <li><a href="http://copbm4cbmpa-com-br.umbler.net/masterrelatoriosvtrs.php">Vtr's</a></li>
+                <li><a href="http://copbm4cbmpa-com-br.umbler.net/masterrelatoriosembarcacoes.php">Embarcações</a></li>
+                </ul>
+              </li>  
+
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+     
+      <li><a href=" http://copbm4cbmpa-com-br.umbler.net/logout.php "><span class="glyphicon glyphicon-log-in"></span> Sair</a></li>
+    </ul>
+  </div>
+</nav>
+
+</body>
+</html>

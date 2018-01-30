98cca05e24c260eb6c5ae1428fef86df703ab732
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-01-30 11:35:42 -0300

    s

--- mastercadastro-embarcacoes.php
+++ mastercadastro-embarcacoes.php
@@ -130,8 +130,8 @@ $result = mysqli_query($conn, $sql);
     
     <td class="actions text-right">
       <a href="http://copbm4cbmpa-com-br.umbler.net/masterviewembarcacoes.php?id=<?php echo $row['id']; ?>"button type="button" class="btn btn-default"><i class="fa fa-eye"></i> Visualizar</a>
-      <a href="mastereditarembarcacoe.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default"><i class="fa fa-pencil"></i> Editar</a>
-      
+      <a href="mastereditarembarcacoes.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default"><i class="fa fa-pencil"></i> Editar</a>
+
       <a href="http://copbm4cbmpa-com-br.umbler.net/excluirembarcacoes.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default" >
          <i class="fa fa-trash"></i> Excluir</a>
     </td>

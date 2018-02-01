5c9e3f0412cf918df0445dd2a11de3d73295dcee
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-02-01 10:45:55 -0300

    view vtr

--- viewvtr.php
+++ viewvtr.php
@@ -76,7 +76,7 @@ $id = $_GET['id'];
 ?>
 
 <?php
-$sql = "SELECT id, ubm, prefixo, placa, renavam, situacao, ocorrencia, nome, rg, defeito, reg_date, cargo, marca,observacoes, chassi, modelo, origem, ano_fab FROM materialoperacional where id=$id ";
+$sql = "SELECT id, ubm, prefixo, placa, renavam, situacao, ocorrencia, nome, rg, defeito, reg_date, cargo,observacoes, chassi, modelo, origem, ano_fab FROM materialoperacional where id=$id ";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
@@ -85,6 +85,90 @@ $row = mysqli_fetch_assoc($result);
   <div class="jumbotron">
   <h2>Visualizaçâo Detalhada do Material</h2>    
     <br>
+
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">#</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">UBM:</th>
+      <th><?php echo $row['ubm']; ?></th>
+    </tr>
+    <tr>
+      <th scope="row"> Prefixo:</th>
+      <td><?php echo $row['prefixo']; ?> </td>
+    
+    </tr>
+    <tr>
+      <th scope="row">Placa:</th>
+      <td><?php echo $row['placa']; ?></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">Renavam</th>
+      <td><?php echo $row['renavam'];?></td>
+      
+    </tr>
+
+    <tr>
+      <th scope="row">Chassi:</th>
+      <td><?php echo $row['chassi']; ?></td>
+    </tr>
+
+    <tr>
+      <th scope="row">Modelo:</th>
+      <td><?php echo $row['modelo']; ?></td>
+    </tr>
+
+    <tr>
+    <th scope="row">Origem</th>
+    <td><?php echo $row['origem']; ?></td>    
+    </tr>
+  
+  <tr>
+    <th scope="row">Ano de Fabricação</th>
+    <td><?php echo $row['ano_fab']; ?></td>
+  </tr>
+
+    <tr>
+      <th scope="row">Situação:</th>
+      <td><?php echo $row['situacao']; ?></td>
+    </tr>
+  <?php if (!$row['ocorrencia']==NULL): ?>
+    <tr>
+      <th scope="row">Ocorrência:</th>
+      <td><?php echo $row['ocorrencia']; ?></td>
+    </tr>
+    <tr>
+      <?php endif ?>
+  <?php     if (!$row['defeito']==NULL): ?>  
+      <th scope="row">Defeito:</th>
+      <td><?php echo $row['defeito'];  ?></td>
+    </tr>
+  
+<?php endif;?>
+   <?php if (!$row['observacoes']==NULL): ?>
+
+    <tr>
+      <th scope="row">Observações</th>
+      <td><?php echo $row['observacoes']; ?></td>
+    </tr>
+
+   <?php endif ?>
+
+
+
+  </tbody>
+</table>
+      
+
+
  	 <div class="row">
     <div class="col-lg-12">
    

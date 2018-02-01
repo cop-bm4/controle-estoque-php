2ddcd4dfa6e78444786adbecdd5a25ff00a3eaf5
cop-bm4 <34241427+cop-bm4@users.noreply.github.com>
2018-02-01 11:17:51 -0300

    s

--- up.php
+++ up.php
@@ -5,14 +5,14 @@ require 'db.php';
 $material = "ABAFADOR INCENDIO";
 $rp = "INFORMAR";
 $nserie = "INFORMAR";
-$situacao = "Inoperante";
+$situacao = "Operante";
 $ocorrencia;
 $marca= "INFORMAR";
 $ubm = "5 GBM";
 $nome = "Joao Luz";
 $rg = "6436847";
 $cargo = "vc";
-$defeito="SEM CABO"; 
+$defeito; 
 $observacoes; 
 
 
@@ -24,10 +24,10 @@ $observacoes;
 for ($i=0; $i <26 ; $i++) {
 
 
-// $sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia, ubm, nome, rg, cargo, defeito, marca, observacoes )
-// VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia','$ubm','$nome','$rg', '$cargo', '$defeito', '$marca', '$observacoes')";
+ $sql = "INSERT INTO materialoperacional (material, rp, nserie, situacao, ocorrencia, ubm, nome, rg, cargo, defeito, marca, observacoes )
+ VALUES ('$material', '$rp', '$nserie', '$situacao', '$ocorrencia','$ubm','$nome','$rg', '$cargo', '$defeito', '$marca', '$observacoes')";
 
-$sql= "UPDATE materialoperacional SET situacao='$situacao', defeito='$defeito' WHERE material='ABAFADOR INCENDIO' ";
+//$sql= "UPDATE materialoperacional SET situacao='$situacao', defeito='$defeito' WHERE material='ABAFADOR INCENDIO' ";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
     echo $i;

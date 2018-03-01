dc0b27d825d276616316c61cd993c0a87034d0d8
João Luz <joao_aluz@hotmail.com>
2018-03-01 11:21:09 -0300

    bm4 a

--- validalogin.php
+++ validalogin.php
@@ -35,7 +35,7 @@ header('location:home.php');
 
 
 }
-elseif ( mysqli_num_rows($result)>0 and $lg='master' or $lg='cprodrigues' or $lg='stedson' or $lg='tcel.helton' or $lg='cop'  and $lg!='bm4'){
+if ( mysqli_num_rows($result)>0 and $lg='master' or $lg='cprodrigues' or $lg='stedson' or $lg='tcel.helton' or $lg='cop'  and $lg!='bm4'){
 	$_SESSION['login'] = $login;
     $_SESSION['senha'] = $senha;
     $_SESSION['ubm']   = $ubm;
@@ -43,8 +43,8 @@ elseif ( mysqli_num_rows($result)>0 and $lg='master' or $lg='cprodrigues' or $lg
 
 
 }
-elseif( mysqli_num_rows($result)>0 and $lg='bm4' and $lg!='master' and $lg!='cprodrigues' and $lg!='stedson' and $lg!='tcel.helton' and $lg!='cop' ){
-$_SESSION['login'] = $login;
+if( mysqli_num_rows($result)>0 and $lg='bm4' and $lg!='master' and $lg!='cprodrigues' and $lg!='stedson' and $lg!='tcel.helton' and $lg!='cop' ){
+	$_SESSION['login'] = $login;
     $_SESSION['senha'] = $senha;
     $_SESSION['ubm']   = $ubm;
     header('location:bm4.php'); 

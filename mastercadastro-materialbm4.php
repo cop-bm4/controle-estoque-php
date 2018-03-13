7eacc23ec32d0e31b2b5a0d70b5e61bb189876d0
João Luz <joao_aluz@hotmail.com>
2018-03-13 07:52:42 -0300

    sem botao excluir

--- mastercadastro-materialbm4.php
+++ mastercadastro-materialbm4.php
@@ -136,8 +136,8 @@ $result = mysqli_query($conn, $sql);
 -->
       <!-- <a href="mastereditar.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default"><i class="fa fa-pencil"></i> Editar</a> -->
       <!-- <a href="http://copbm4cbmpa-com-br.umbler.net/excluir.php?id=<?php echo $row['id']; ?>" button type="button" class="btn btn-default" > -->
-         <i class="fa fa-trash"></i> Excluir</a>
-      <a href="http://copbm4cbmpa-com-br.umbler.net/cautela.php?id=<?php echo $row['id']; ?>" button type="button" class ="btn btn-default"><i class="fa fa-pencil"> </i> Cautela </a>   
+        <!-- <i class="fa fa-trash"></i> Excluir</a> -->
+      <a href="http://copbm4cbmpa-com-br.umbler.net/cautela.php?id=<?php echo $row['id']; ?>" button type="button" class ="btn btn-default"><i class="fa fa-pencil"> </i> Cautela </a>
     </td>
   </tr>
 

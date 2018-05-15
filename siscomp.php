f972aecf58269700822bae5dcb0873c45e463093
João Luz <joao_aluz@hotmail.com>
2018-05-15 11:42:33 -0300

    assa

---
+++ siscomp.php
@@ -0,0 +1,85 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login siscomp</title>
+	 <meta charset="utf-8">
+  
+  
+<!--mod mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!---->
+  <!-- include bibliotecas bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!---->
+
+<!--fixed footer -->
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: black;
+   text-align: center;
+   font-size: 12px;
+}
+</style>
+<!-- -->
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"> SISCOMP </a>
+    </div>
+    <ul class="nav navbar-nav">
+   	
+   </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="http://bm4cop-org.umbler.net/login.html"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+      
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>SISCOMP</h1> 
+    <p class="text-danger">Sistema de controle de material operacional</p> 
+
+  </div>
+  </div>
+
+
+
+
+<div class="container">
+
+<form method="post" class="input-group" action="validalogin.php" id="formlogin" name="formlogin">
+
+ <div class="input-group input-group-lg">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+    <input id="login" type="text" class="form-control" name="login" placeholder="login">
+  </div>
+  <div class="input-group input-group-lg">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+    <input id="senha" type="password" class="form-control" name="senha" placeholder="senha">
+  </div>
+  <br>
+  <input type="submit" class="btn  btn-lg" value="LOGAR	"  />
+
+  </div>
+</form>
+
+</div>
+
+<div class="footer">
+  <p>© 2018 BM4/COP CBM-PA    V.C João Luz </p>
+</div>
+
+</body>
+</html>
+

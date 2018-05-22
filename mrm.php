a1537d6b09d9e8fc182de828cdf1249e5f33f233
João Luz <joao_aluz@hotmail.com>
2018-05-22 11:20:07 -0300

    pagina mrm

---
+++ mrm.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MRM</title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+  <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: black;
+   text-align: center;
+   font-size: 12px;
+}
+</style>
+</head>
+<body>
+
+	<div class="jumbotron text-center">
+  <h1>BM4</h1>
+  <h2>Mapa de registro de missão.</h2>
+</div>
+
+
+
+<div class="footer">
+  <p>© 2018 BM4/COP CBM-PA    V.C João Luz </p>
+</div>
+</body>
+</html>

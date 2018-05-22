cf765b4030db75b97d3ad8b82deca8f964fa7def
João Luz <joao_aluz@hotmail.com>
2018-05-22 11:31:06 -0300

    primeira missao

--- mrm.php
+++ mrm.php
@@ -2,13 +2,12 @@
 <html>
 <head>
 	<title>MRM</title>
-	 <meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
   <style>
 .footer {
    position: fixed;
@@ -33,12 +32,12 @@
   <div class="row">
     <div class="col-sm-4">
       <h3>Evento: </h3>
-      <p class="font-weight-bold">Data/hora: </p> <p>ech</p>
-      <p class="font-weight-bold">Local: </p> <p>Local </p>
-      <p class="font-weight-bold">Destino</p> <p>Destino</p>
-      <p class="font-weight-bold">vtr</p> <p>vtr</p>
-      <p class="font-weight-bold">condutor</p> <p>condutor</p>
-      <p class="font-weight-bold">protocolo</p> <p>protocolo</p>
+      <p class="font-weight-bold">Data/hora: </p> 
+      <p class="font-weight-bold">Local: </p> 
+      <p class="font-weight-bold">Destino: </p> 
+      <p class="font-weight-bold">vtr: </p> 
+      <p class="font-weight-bold">condutor: </p> 
+      <p class="font-weight-bold">protocolo: </p> 
     </div>
   </div>
 </div>

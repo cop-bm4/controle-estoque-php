92b6e51d59221322dd3c81c223d88500435e046d
João Luz <joao_aluz@hotmail.com>
2018-05-30 12:53:38 -0300

    valida login

---
+++ mrm/validalogin.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+
+
+
+require "db.php";
+
+if ($conn->maxdb_connect_error()) {
+	die("Connection failed". $conn->connect_error);
+}
+
+
+
+$sql= "SELECT login, senha FROM materialoperacional WHERE 'login' = '$login' AND 'senha'='$senha'";
+
+$result =  mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+
+
+
+?>

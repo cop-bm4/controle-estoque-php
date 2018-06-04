f9ec9abf0633042badc47c7660ec9ee491938121
João Luz <joao_aluz@hotmail.com>
2018-06-04 08:56:20 -0300

    credencias

--- mrm/validalogin.php
+++ mrm/validalogin.php
@@ -19,11 +19,16 @@ $sql= "SELECT login, senha FROM usuarios WHERE 'login' = '$login' AND 'senha'='$
 $result =  mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
+
+$lg
 if (mysqli_num_rows($result)>0) {
+$_SESSION['login'] = $login;
+$_SESSION['senha'] = $senha;
 	header('location:home.php');
 }
+
 else{
-	header('location:credenciasinvalidas.php');
+	header('location:credenciaisinvalidas.php');
 	
 }
 

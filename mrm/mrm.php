d31504ccfd961d19a499e4bcf894d8d7d3875797
João Luz <joao_aluz@hotmail.com>
2018-06-04 08:11:26 -0300

    footer.php

--- mrm/mrm.php
+++ mrm/mrm.php
@@ -110,12 +110,15 @@ require 'head.php';
 </div>
 
 
-
+<!--
 
 <div class="footer">
   <p>© 2018 BM4/COP CBM-PA    V.C João Luz </p>
 </div>
-
+-->
+<?php
+require 'footer.php';
+?>
 </body>
 
 </html>

496ed34cc3e70250365efccc09cc2fd10e23e7da
João Luz <joao_aluz@hotmail.com>
2018-06-04 09:25:04 -0300

    ssa

--- mrm/validalogin.php
+++ mrm/validalogin.php
@@ -19,8 +19,6 @@ $sql= "SELECT login, senha FROM usuarios WHERE 'login' = '$login' AND 'senha'='$
 $result =  mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
-
-$lg
 if (mysqli_num_rows($result)>0) {
 $_SESSION['login'] = $login;
 $_SESSION['senha'] = $senha;

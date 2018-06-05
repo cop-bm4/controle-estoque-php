1a806838a4d4a809d45a43c732b947e9f625d2b1
João Luz <joao_aluz@hotmail.com>
2018-06-05 11:23:49 -0300

    DB.PHP

---
+++ mrm/missao/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = "mysql857.umbler.com";
+$username = "copbm4";
+$password = "copbm4123";
+$db = "copbm4"; 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+   
+  ?>

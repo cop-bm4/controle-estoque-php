d5dc8cea3ca0500bc0120df1ba86231439f4bf29
João Luz <joao_aluz@hotmail.com>
2018-06-05 08:38:35 -0300

    s

--- mrm/home.php
+++ mrm/home.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Novo</title>
- <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <?php 
-
+require 'head.php';
 require 'credenciais.php';
  ?>
 </head>
@@ -29,7 +23,9 @@ require 'credenciais.php';
 		<div class="col-sm-4">
 			<div class="alert alert-danger" role="alert">
 				<h3><strong>Missões</strong></h3>
-				<button type="button" class="btn btn-success" class="glyphicon glyphicon-plus-sign" <a href="/mrm/missao/cadastro.php"></a></button>
+				 <a href="#" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-plus"></span> Plus 
+        </a>
 						
 			</div>
 		</div>

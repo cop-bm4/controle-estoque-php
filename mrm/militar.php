a0dcc9daeb5f09389185b6d110e00c7fca65f956
João Luz <joao_aluz@hotmail.com>
2018-06-05 07:37:34 -0300

    sa

---
+++ mrm/militar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+	require 'db.php';
+	require 'head.php';
+	require 'credenciais.php';
+?>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="http://bm4cop-org.umbler.net/mrm/mrm.php">MRM</a>
+  
+</nav>
+
+<?php
+
+$sql= "SELECT nome, graduacao, "
+
+
+
+?>
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6">
+			<form action="#" method="post" id="needs-validation">
+				
+				
+			</form>
+		</div>
+	</div>
+
+</div>
+
+
+
+<?php
+
+require 'footer.php';
+?>
+</body>
+</html>
---
+++ mrm/viatura/cadastro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+require '/mrm/head.php';
+?>
+
+</head>
+
+
+<body>
+
+
+
+<?php 
+require '/mrm/footer.php';
+?>
+</body>
+</html>

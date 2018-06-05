ca8a66abc968c34303eac4b667e19de5cc0096a5
João Luz <joao_aluz@hotmail.com>
2018-06-05 09:29:27 -0300

    s

--- mrm/home.php
+++ mrm/home.php
@@ -46,8 +46,8 @@ require 'credenciais.php';
 		<div class="col-sm-4">
 			<div class="alert alert-success"  role="alert">
 				<h3 ><strong>Viaturas</strong></h3>
-					 <a href="/mrm/viatura/cadastro.php" class="btn btn-info btn-lg" class="text-center"><span class="glyphicon glyphicon-plus"></span> Nova </a><a href="#" class="btn btn-info btn-lg" class="text-center">
-         				 <span class="glyphicon glyphicon-pencil"></span> Editar </a><a href="#" class="btn btn-info btn-lg" class="text-center">
+					 <a href="/mrm/viatura/cadastro.php" class="btn btn-success btn-lg" class="text-center"><span class="glyphicon glyphicon-plus"></span> Nova </a><a href="#" class="btn btn-info btn-lg" class="text-center">
+         				 <span class="glyphicon glyphicon-pencil"></span> Editar </a><a href="#" class="btn btn-danger btn-lg" class="text-center">
          				 <span class="glyphicon glyphicon-trash"></span> Excluir </a>			 
 			</div>
 		</div>
--- mrm/viatura/cadastro.php
+++ mrm/viatura/cadastro.php
@@ -12,6 +12,48 @@ require 'credenciais.php';
 
 <body>
 
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="bm4cop-org.umbler.net/mrm/home.php">Home</a>
+  
+</nav>
+
+
+<form class="form-control" action="#" method="post">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-4">
+						<div class="input-group">
+							<span class="input-group-addon" id="inputGroup-sizing-default">Prefixo</span>
+							<input type="text" name="prefixo" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" required>
+						</div>
+					</div>
+						
+					<div class="col-4">
+						<div class="input-group">
+							<span class="input-group-addon" id="inputGroup-sizing-default">Placa</span>
+							<input type="text" name="placa" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" required>
+						</div>
+
+					</div>	
+
+				</div>
+			</div>
+</form>
+
+
+
+<?php
+require '/public/db.php';
+$prefixo = $_POST['prefixo'];
+$placa = $_POST['placa'];
+
+
+
+
+
+
+?>
 
 
 <?php 

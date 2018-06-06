fbb62f2af3984deccfa7ed10f2ca49fca6970b4c
João Luz <joao_aluz@hotmail.com>
2018-06-06 12:07:00 -0300

    data_caadastro missao

--- mrm/missao/cadastro.php
+++ mrm/missao/cadastro.php
@@ -37,6 +37,14 @@ require 'db.php';
 				</div>
 
 				<div class="form-group">
+					<label for="inputEmail3" class="col-sm-2 control-label">Localização</label>
+					<div class="col-sm-10">
+						<input type="text" name="localizacao" class="form-control" id="inputEmail3" placeholder="Localização">
+					</div>
+				</div>
+
+
+				<div class="form-group">
 					<label for="inputPassword3" class="col-sm-2 control-label">Data e Hora</label>
 					<div class="col-sm-4">
 						<div class="input-group date data_formato" data-date-format="dd/mm/yyyy HH:ii:ss">
@@ -69,8 +77,39 @@ require 'db.php';
 						</select>
 
 					<?php  endif; ?>
+
 					</div>
 				</div>
+
+				
+				<div class="form-group">
+					<label for="inputPassword3" class="col-sm-2 control-label">Viatura</label>
+					<div class="col-sm-4">
+						<select class="form-control form-control-lg" name="cod_viatura">
+						<?php
+						$sql_viatura = "SELECT cod_viatura,prefixo FROM viaturas";
+						$result_viatura = mysqli_query($conn, $sql);	
+
+						?>	
+						<?php if (mysqli_num_rows($result_viatura)>0): ?>
+
+
+						<?php while($row_viatura=mysqli_fetch_assoc($result_viatura)):?>
+
+						<option value="<?php echo $row_viatura['cod_viatura'];?>"><?php echo $row_viatura['prefixo']; ?></option>
+
+						<?php endwhile; ?>	
+						
+						</select>
+
+					<?php  endif; ?>
+
+					</div>
+				</div>
+
+				
+
+
 
 				<div class="form-group">
 					<div class="col-sm-offset-2 col-sm-10">

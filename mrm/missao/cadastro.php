68545bef0ac302e5df9699e706566111db0a93cc
João Luz <joao_aluz@hotmail.com>
2018-06-06 11:41:50 -0300

    data_caadastro missao

--- mrm/missao/cadastro.php
+++ mrm/missao/cadastro.php
@@ -57,7 +57,7 @@ require 'db.php';
 						$result = mysqli_query($conn, $sql);	
 
 						?>	
-						<?php while($row_militar=mysqli_num_rows($result)>0):
+						<?php while($row_militar=mysqli_num_rows($result)):
 						?>
 						<option><?php echo $row_militar['nome']; ?></option>
 						<?php endwhile; ?>	

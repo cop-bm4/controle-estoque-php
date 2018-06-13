14c4289dc3bb618bdcdce63ca3dc7f75ac0a9e55
João Luz <joao_aluz@hotmail.com>
2018-06-13 11:32:50 -0300

    modal

--- mrm/missao/concluir.php
+++ mrm/missao/concluir.php
@@ -11,7 +11,23 @@ require 'db.php';
 <link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$(".launch-modal").click(function(){
+		$("#myModal").modal({
+			backdrop: 'static'
+		});
+	}); 
+});
+</script>
+<style type="text/css">
+    .bs-example{
+    	margin: 20px;
+    }
+</style>
 
 </head>
 <body>
@@ -25,23 +41,31 @@ require 'db.php';
 
 
 
-
-<!-- Button trigger modal -->
-<div id="myModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content">
-      ...
+<div class="bs-example">
+    <!-- Button HTML (to Trigger Modal) -->
+    <input type="button" class="btn btn-lg btn-primary launch-modal" value="Launch Demo Modal">
+    
+    <!-- Modal HTML -->
+    <div id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Confirmation</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Do you want to save changes you made to document before closing?</p>
+                    <p class="text-warning"><small>If you don't save, your changes will be lost.</small></p>
+                    <p class="text-info"><small><strong>Note:</strong> If you click on the black area outside of this modal window it will not hide.</small></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
-
-
-<script>
-	$(document).ready(function() {
-   $('#myModal').modal('show');
-	});
-
-</script>
 
 </body>
 </html>

5b05f79b240952d41c0865a96bf9e7173c1dc61f
João Luz <joao_aluz@hotmail.com>
2018-06-14 09:28:26 -0300

    ocncluir

---
+++ mrm/logout.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+    // se você possui algum cookie relacionado com o login deve ser removido
+    session_destroy();
+    header("location: http://bm4cop-org.umbler.net/mrm/mrm.php");
+
+
+?>

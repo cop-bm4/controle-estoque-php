9a1ec493a8311c9a1b183d5f09e1173303ff7b7a
João Luz <joao_aluz@hotmail.com>
2018-06-15 12:30:08 -0300

    excluir

--- mrm/militar/exclui.php
+++ mrm/militar/exclui.php
@@ -4,13 +4,11 @@ $id =  $_GET['id'];
 
 require 'db.php';
 require 'credenciais.php';
-$sql = "DELETE  FROM militar WHERE cod_mitilitar= '$id'";
+$sql = "DELETE FROM militar WHERE cod_mitilitar= '$id'";
 $result = mysqli_query($conn, $sql);
 
 if ($conn->query($sql)===TRUE) {
 	header('location: /mrm/militar/excluir.php'); 
-
-
 }
 
 

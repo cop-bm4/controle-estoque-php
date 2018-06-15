017701a7d2672078bd28d7161874e8b09ffbb8eb
João Luz <joao_aluz@hotmail.com>
2018-06-15 12:23:31 -0300

    pagina de sql excluir

---
+++ mrm/militar/exclui.php
@@ -0,0 +1,18 @@
+<?php
+$id =  $_GET['id'];
+
+
+require 'db.php';
+require 'credenciais.php';
+$sql = "DELETE  FROM militar WHERE cod_mitilitar= '$id'";
+$result = mysqli_query($conn, $sql);
+
+if ($conn->query($sql)===TRUE) {
+	header('location: /mrm/militar/excluir.php'); 
+
+
+}
+
+
+
+?>

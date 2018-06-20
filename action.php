a955f915f9b8a627f505752c944737066ea8ff00
João Luz <joao_aluz@hotmail.com>
2018-06-20 08:37:40 -0300

    as

--- action.php
+++ action.php
@@ -16,50 +16,29 @@ $ubm = $_REQUEST['ubm'];
 ?>
 
 <?php
-/* Carrega a classe DOMPdf */
-require_once("/operacao_verao/dompdf/dompdf_config.inc.php");
+   //Inclui a classe 'class.ezpdf.php'
+   include("pdf-php/class.ezpdf.php");
 
-/* Cria a instância */
-$dompdf = new DOMPDF();
+   //Instancia um novo documento com o nome de pdf
+   $pdf = new Cezpdf();
 
-/* Carrega seu HTML */
-$dompdf->load_html('<!doctype html>
-<html>
+   //Seleciona a fonte que será usada. As fontes estão localizadas na pasta "pdf-php/fonts". Use a de sua preferencia.
+   $pdf -> selectFont('pdf-php/fonts/Helvetica.afm');
 
-<head>
-<meta charset="UTF-8">
-<title>Atenção, apenas um teste</title>
-<style>
-* { font-family: sans-serif; }
-</style>
-</head>
+   //Chama o método "ezText".
+   //No 1° parametro passa o texto do documento
+   //No 2° parametro define o tamanho da fonte.
+   //No 3° parametro é do tipo array. A seguir uma explicação desse 3° parametro:
 
-<body>
+   // justification => seta a posição de um label, pode ser center, right, left, aright, ou aleft
+   // leading = > define o tamanho que cada linha usará para se mostrada, deverá  ser um int
+   // spacing => define o espaçamento entrelinhas, deverá ser um float
+   // você pode usar apenas leading ou apenas spacing, nunca os dois
 
-<header>
-    <h1>Atenção, isso é apenas um teste</h1>
-    <p>Estou dando um exemplo sobre como utilizar a dompdf.
-    Meu nome é <b>Luiz Otávio</b> E trabalho no blog
-    <a href="http://www.todoespacoonline.com/w/">todoespacoonline.com/w</a>. Minha foto segue abaixo:
-    </p>
+   $pdf -> ezText('DevMedia Group!', 20, array(justification => 'center', spacing => 2.0));
+   $pdf -> ezText('Olá Pessoal. Obrigado por estarem acompanhando mais este artigo!', 15, array(justification => 'left', spacing => 3.0));
+   $pdf -> ezText('Acessem o portal da DevMedia Group: www.devmedia.com.br!', 10, array(justification => 'right', spacing => 1.0));
 
-    <p style="text-align:center">
-        <img src="foto.jpg">
-    </p>
-</header>
-
-</body>
-
-</html>');
-
-/* Renderiza */
-$dompdf->render();
-
-/* Exibe */
-$dompdf->stream(
-    "saida.pdf", /* Nome do arquivo de saída */
-    array(
-        "Attachment" => true /* Para download, altere para true */
-    )
-);
+   //Gera o PDF
+   $pdf -> ezStream();
 ?>

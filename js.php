9aee30f4c0c8bd95ef413f8864d38be319c5f136
João Luz <joao_aluz@hotmail.com>
2018-06-20 10:10:05 -0300

    js

--- js.php
+++ js.php
@@ -3,14 +3,15 @@
 <head>
   <script >
   function show(){
-    document.getElementById('demo').InnerHTML = "Teste function";
-  }</script>
+    document.getElementById('demo').innerHTML = "Teste function";
+  }
+  </script>
 
 </head>
 
 <body>
 
-<h2>What Can JavaScript Do?</h2>
+<h2>What Can JavaScript Do?s</h2>
 
 <p id="demo">JavaScript can change HTML content ss.</p>
 

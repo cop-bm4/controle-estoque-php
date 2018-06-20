66d89637fe1a868857b4650cdab88cd13a8a2126
João Luz <joao_aluz@hotmail.com>
2018-06-20 11:47:01 -0300

    jquery plugin

--- js.php
+++ js.php
@@ -1,18 +1,49 @@
-<!DOCTYPE html>
 <html>
 <head>
-  <script src="/script/script.js" >
-  </script>
+<tittle>JQ</tittle>
 
 </head>
 
 <body>
+  <p><label><input type="checkbox" id="example1"> Are you sure?</label></p>
+  <p class="conditional" data-condition="#example1">
+    <label><input type="checkbox" name="example2"> Really super sure?</label>
+  </p>
+  <p class="conditional" data-condition="#example1 && example2">
+    <label>Then type "yay": </label>
+    <input type="text" id="example3" placeholder="yay">
+  </p>
+  <!-- This will be shown only if BOTH examle1 and examle2 are checked AND 'yay' typed in examle3 -->
+  <p class="conditional msg"
+     data-condition="#example1 && example2 && #example3 == 'yay'">
+     Both are selected and YAY is typed!
+  </p>
+  <p>
+    <label>Pick two or three:</label>
+    <select class="select" name="example5">
+      <option>....</option>
+      <option value="one">One!</option>
+      <option value="two">Two!</option>
+      <option value="three">Three!</option>
+      <option value="four">Four!</option>
+    </select>
+  </p>
+  <div class="conditional msg"
+       data-condition="['two','three'].includes(example5)">
+     See?! It works with selects!
+   </div>
 
-<h2>What Can JavaScript Do?s</h2>
 
-<p id="demo">JavaScript can change HTML content ss.</p>
 
-<button type="button" onclick="show()">Click Me!</button>
 
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js"
+          integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
+          crossorigin="anonymous">
+  </script>
+  <script src="/jQuery-Plugin-For-Conditional-Form-Fields-conditionize-js/conditionize.flexible.jquery.js"></script>
+  <script>$('.conditional').conditionize();
+</script>
 </body>
+
+
 </html>
--- script/script.js
+++ script/script.js
@@ -3,3 +3,7 @@ function show() {
 
 
 }
+function termo( ){
+
+
+}

2ba302c206499cca4b66310653da4170d7447a48
João Luz <joao_aluz@hotmail.com>
2018-06-25 09:26:20 -0300

    int

--- act.php
+++ act.php
@@ -69,7 +69,7 @@ echo $qnt_labial;
         <input value="<?php echo $cargo_recebedor." ".$recebedor ?>" type="text" name="recebedor" class="form-control" id="inputEmail3" readonly>
       </div>
     </div>
-POST
+REQUEST
 <?php if ($qnt_oculos>2):?>
 <h2>deu</h2>
 <?php endif; ?>

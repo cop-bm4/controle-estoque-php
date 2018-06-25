03490f8d0ea42f4c94bf1f65b04ead9a6e2097df
João Luz <joao_aluz@hotmail.com>
2018-06-25 08:32:22 -0300

    pagina it

--- act.php
+++ act.php
@@ -3,7 +3,8 @@ require 'db.php';
 
 $pagador = $_POST['pagador'];
 $cargo_pagador = $_POST['cargo_pagador'];
-$recebdor = $_POST['cargo_recebedor'];
+$recebedor = $_POST['recebedor'];
+$cargo_recebedor = $POST['cargo_recebedor'];
 $qnt_oculos = $POST['q_oculos'];
 $qnt_short = $POST['q_short'];
 $qnt_camisa = $POST['q_camisa'];
@@ -48,12 +49,23 @@ $qnt_labial = $POST['q_labial'];
 <h4>Nominal</h4>
 <form class="form-horizontal" action="action.php" method="post">
 
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label" >Responsável</label>
-    <div class="col-sm-10">
-      <input value="<?php echo $pagador ?>" type="text" name="pagador" class="form-control" id="inputEmail3" readonly>
+    <div class="form-group">
+      <label for="inputEmail3" class="col-sm-2 control-label" >Pagador</label>
+      <div class="col-sm-10">
+        <input value="<?php echo $cargo_pagador." ".$pagador ?>" type="text" name="pagador" class="form-control" id="inputEmail3" readonly>
+      </div>
     </div>
-  </div>
+
+    <div class="form-group">
+      <label for="inputEmail3" class="col-sm-2 control-label" >Recebedor</label>
+      <div class="col-sm-10">
+        <input value="<?php echo $cargo_recebedor." ".$recebedor ?>" type="text" name="recebedor" class="form-control" id="inputEmail3" readonly>
+      </div>
+    </div>
+
+<?php if (isset($_POST['q_oculos'])):?>
+<h2>deu</h2>
+<?php endif; ?>
 
 
 

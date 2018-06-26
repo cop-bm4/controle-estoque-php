73fb9a077d32b64aea4badd163657468ecfa0429
João Luz <joao_aluz@hotmail.com>
2018-06-26 12:19:46 -0300

    as

--- action.php
+++ action.php
@@ -21,6 +21,12 @@ $dompdf = new DOMPDF();
 $dompdf->load_html('<p>
 <html>
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 <h1>teste</h1>
 </head>
 
